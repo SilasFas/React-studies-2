1d5606ed8ba3668732314eb3be61d8dbc9bbe659
Silas <silasfas@hotmail.com>
2023-04-30 00:08:11 -0300

    Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { HelloWorld } from './components/HelloWorld';
 
 function App() {
   return (
     <div className="App">
       <h1>Hey there!</h1>
       <h1>We start here!</h1>
+      <HelloWorld />
     </div>
   );
 }
---
+++ src/components/HelloWorld.js
@@ -0,0 +1,16 @@
+import { Frase } from "./Frase"
+
+export const HelloWorld = () => {
+    return (
+        <div>
+            < Frase />
+            <h1>Meu primeiro componente</h1>
+            < Frase />
+            < Frase />
+        </div>
+    )
+}
+
+// function HelloWorld() {
+// }
+// export default HelloWorld
