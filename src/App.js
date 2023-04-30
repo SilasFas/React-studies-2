e53841141b391a5e88ff432da4ac184da0f8f2fd
Silas <silasfas@hotmail.com>
2023-04-30 01:13:44 -0300

    CSS no REACT

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { HelloWorld } from './components/HelloWorld';
 import { SayMyName } from './components/SayMyName';
 import { Pessoa } from './components/Pessoa';
+import { Frase } from './components/Frase';
+
 
 function App() {
   const nome = 'Ana'
@@ -10,13 +12,14 @@ function App() {
       <SayMyName nome='Silas' />
       <SayMyName nome='Pedro' />
       <SayMyName nome={nome} />
-      
       <Pessoa
         nome='Silas'
         idade={45}
         profissao='Programador'
         foto='https://via.placeholder.com/150'
       />
+      <Frase />
+      <Frase />
     </div>
   );
 }
